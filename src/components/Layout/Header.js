c1d7292ab096f90b046de916e4a387bb36487a38
Sagar Mittal <mittalsagar006@gmail.com>
2022-12-02 16:23:49 +0530

    made Header & Layout of the app

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+import HeaderCartButton from "./HeaderCartButton";
+import mealsImage from '../../assets/meals.jpg'
+import classes from './Header.module.css'
+
+const Header = props => {
+  return <>
+    <header className={classes.header}>
+      <h1>ReactMeals</h1>
+      <HeaderCartButton />
+    </header>
+    <div className={classes['main-image']}>
+      <img src={mealsImage} alt="A table full of dishes"/>
+    </div>
+  </>
+}
+
+export default Header;
