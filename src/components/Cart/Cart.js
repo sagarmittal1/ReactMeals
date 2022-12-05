ae8b06d6f89be7560bf29fcbfe4c939b03a2e1bc
Sagar Mittal <mittalsagar006@gmail.com>
2022-12-05 16:06:26 +0530

    made Cart component

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,30 @@
+import Modal from '../UI/Modal';
+import classes from './Cart.module.css';
+
+const Cart = (props) => {
+  const cartItems = (
+    <ul className={classes['cart-items']}>
+      {[{ id: 'm1', name: 'Shahi Paneer', quantity: 1, price: 150 }].map(
+        (item) => (
+          <li>{item.name}</li>
+        )
+      )}
+    </ul>
+  );
+
+  return (
+    <Modal>
+      {cartItems}
+      <div className={classes.total}>
+        <span>Total Amount:</span>
+        <span>499.99</span>
+      </div>
+      <div className={classes.actions}>
+        <button className={classes['button--alt']}>Close</button>
+        <button className={classes.button}>Order</button>
+      </div>
+    </Modal>
+  );
+};
+
+export default Cart;
