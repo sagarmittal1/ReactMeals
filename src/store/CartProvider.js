f5e0e371dacae149754fa7131060f1731637b443
Sagar Mittal <mittalsagar006@gmail.com>
2022-12-07 15:50:44 +0530

    made cartReducer logic complex to handle same item

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -8,9 +8,28 @@ const defaultCartState = {
 
 const cartReducer = (state, action) => {
   if (action.type === 'ADD') {
-    const updatedItems = state.items.concat(action.item);
     const updatedTotalAmount =
       state.totalAmount + action.item.price * action.item.quantity;
+
+    // first if item exist then find index of it
+    // we have to keep in mind that if it exists then we have to increase quantity of it not add another
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.item.id
+    );
+    const existingCartItem = state.items[existingCartItemIndex];
+    let updatedItems;
+
+    if (existingCartItem) {
+      const updatedItem = {
+        ...existingCartItem,
+        quantity: existingCartItem.quantity + action.item.quantity,
+      };
+      updatedItems = [...state.items];
+      updatedItems[existingCartItemIndex] = updatedItem;
+    } else {
+      updatedItems = state.items.concat(action.item);
+    }
+
     return {
       items: updatedItems,
       totalAmount: updatedTotalAmount,
