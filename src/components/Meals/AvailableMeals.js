4f9890fd8dd73faff7bbc44972efb2f1837ae4f7
Sagar Mittal <mittalsagar006@gmail.com>
2022-12-27 17:45:18 +0530

    added isLoading & httpError state to catch errors

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -6,13 +6,20 @@ import classes from './AvailableMeals.module.css';
 
 const AvailableMeals = () => {
   const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState();
 
   useEffect(() => {
     // we need to use function bcoz we can't use promise in useEffect function
     const fetchMeals = async () => {
       const response = await fetch(
-        'https://react-learning-3e944-default-rtdb.firebaseio.com/meals.json'
+        'https://react-learning-3e944-default-rtdb.firebaseio.com/meals'
       );
+
+      if(!response.ok) {
+        throw new Error('Something went wrong');
+      }
+
       const responseData = await response.json();
 
       const loadedMeals = [];
@@ -27,10 +34,24 @@ const AvailableMeals = () => {
       }
 
       setMeals(loadedMeals);
+      setIsLoading(false);
     };
 
-    fetchMeals();
+    fetchMeals().catch(error => {
+      setIsLoading(false);
+      setHttpError(error.message);
+    });
   }, []);
+
+  if (isLoading) {
+    return <section className={classes.spinner}></section>;
+  }
+
+  if(httpError) {
+    return <section className={classes.mealsError}>
+      <p>{httpError}</p>
+    </section>
+  }
 
   const mealsList = meals.map((meal) => (
     <MealItem
