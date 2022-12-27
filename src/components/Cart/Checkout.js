14fe0aa028cadc3b3f0fe49b9a08c09fea60545a
Sagar Mittal <mittalsagar006@gmail.com>
2022-12-28 03:01:22 +0530

    added states, submitHandler, error-states

--- src/components/Cart/Checkout.js
+++ src/components/Cart/Checkout.js
@@ -1,27 +1,90 @@
+import { useRef, useState } from 'react';
+
 import classes from './Checkout.module.css';
 
+const isEmpty = (value) => value.trim() === '';
+const isSixChars = (value) => value.trim().length === 6;
+
 const Checkout = (props) => {
+  const [formInputsValidity, setFormInputsValidity] = useState({
+    name: true,
+    address: true,
+    city: true,
+    pinCode: true,
+  });
+
+  const nameInputRef = useRef();
+  const addressInputRef = useRef();
+  const pinCodeInputRef = useRef();
+  const cityInputRef = useRef();
+
   const confirmHandler = (event) => {
     event.preventDefault();
+
+    const enteredName = nameInputRef.current.value;
+    const enteredAddress = addressInputRef.current.value;
+    const enteredPinCode = pinCodeInputRef.current.value;
+    const enteredCity = cityInputRef.current.value;
+
+    const enteredNameIsValid = !isEmpty(enteredName);
+    const enteredAddressIsValid = !isEmpty(enteredAddress);
+    const enteredCityIsValid = !isEmpty(enteredCity);
+    const enteredPinCodeIsValid = isSixChars(enteredPinCode);
+
+    setFormInputsValidity({
+      name: enteredNameIsValid,
+      address: enteredAddressIsValid,
+      city: enteredCityIsValid,
+      pinCode: enteredPinCodeIsValid,
+    });
+
+    const formIsValid =
+      enteredNameIsValid &&
+      enteredAddressIsValid &&
+      enteredCityIsValid &&
+      enteredPinCodeIsValid;
+
+    if (!formIsValid) {
+      return;
+    }
   };
+
+  const nameClasses = `${classes.control} ${
+    formInputsValidity.name ? '' : classes.invalid
+  }`;
+  const addressClasses = `${classes.control} ${
+    formInputsValidity.address ? '' : classes.invalid
+  }`;
+  const cityClasses = `${classes.control} ${
+    formInputsValidity.city ? '' : classes.invalid
+  }`;
+  const pinCodeClasses = `${classes.control} ${
+    formInputsValidity.pinCode ? '' : classes.invalid
+  }`;
 
   return (
     <form className={classes.form} onSubmit={confirmHandler}>
-      <div className={classes.control}>
+      <div className={nameClasses}>
         <label htmlFor="name">Name</label>
-        <input type="text" id="name" />
+        <input type="text" id="name" ref={nameInputRef} />
+        {!formInputsValidity.name && <p>Please enter a valid name!</p>}
       </div>
-      <div className={classes.control}>
+      <div className={addressClasses}>
         <label htmlFor="address">Address</label>
-        <input type="text" id="address" />
+        <input type="text" id="address" ref={addressInputRef} />
+        {!formInputsValidity.address && <p>Please enter a valid address!</p>}
       </div>
-      <div className={classes.control}>
+      <div className={pinCodeClasses}>
         <label htmlFor="pincode">Pin Code</label>
-        <input type="text" id="pincode" />
+        <input type="text" id="pincode" ref={pinCodeInputRef} />
+        {!formInputsValidity.pinCode && (
+          <p>Please enter a valid Pin Code (6 characters long).</p>
+        )}
       </div>
-      <div className={classes.control}>
+      <div className={cityClasses}>
         <label htmlFor="city">City</label>
-        <input type="text" id="city" />
+        <input type="text" id="city" ref={cityInputRef} />
+        {!formInputsValidity.city && <p>Please enter a valid city!</p>}
       </div>
       <div className={classes.actions}>
         <button type="button" onClick={props.onCancel}>
